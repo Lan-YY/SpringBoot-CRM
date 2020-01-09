cc61f2bd81666938bf377f113b8e39f8bfd45c30
Administrator <2831040660@qq.com>
2020-01-09 10:31:59 +0800

    使用springboot  Mybatis+Thymeleaf完成客户关系管理系统增删查  拦截器  角色中使用ajax  Jedis基本用法

--- src/test/java/com/thymeleaf/mybatis/ThymeleafApplicationTests.java
+++ src/test/java/com/thymeleaf/mybatis/ThymeleafApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +12,20 @@ public class ThymeleafApplicationTests {
 
     @Test
     public void contextLoads() {
+
+        //1.设置IP地址和端口
+        Jedis jedis = new Jedis("localhost",6379);
+        System.out.println("redis连接成功");
+
+        //2.存储数据
+        jedis.set("k1","测试1");
+        jedis.set("k2","测试2");
+
+        System.out.println(jedis.get("k1"));
+
+        //3.释放资源
+        jedis.close();
+
     }
 
 }
